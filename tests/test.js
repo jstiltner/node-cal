5292e4ab2488c57a5bd74a5935b16ebe95ddb3b0
Jason Stiltner <jasonstiltner@gmail.com>
2016-01-26 12:13:16 -0600

    files ready

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,11 @@
 'use strict';
 const { expect } = require('chai');
+const { execSync } = require('child_process');
 
-describe('test suite', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+describe('cal', () => {
+  it('should handle the current month', () => {
+    const goal = execSync('cal').toString();
+    const output = execSync('./cal.js').toString();
+    expect(output).to.equal(goal);
   });
 });
